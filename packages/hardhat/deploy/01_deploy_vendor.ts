c12764fe09d74393fb9ef1973b53be9665be8a8d
etherian <rianziwalker@gmail.com>
2024-12-13 21:19:58 +0700

    deployer script

--- packages/hardhat/deploy/01_deploy_vendor.ts
+++ packages/hardhat/deploy/01_deploy_vendor.ts
@@ -8,38 +8,33 @@ import { Contract } from "ethers";
  *
  * @param hre HardhatRuntimeEnvironment object.
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const deployVendor: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  /*
-    On localhost, the deployer account is the one that comes with Hardhat, which is already funded.
-
-    When deploying to live networks (e.g `yarn deploy --network goerli`), the deployer account
-    should have sufficient balance to pay for the gas fees for contract creation.
-
-    You can generate a random account with `yarn generate` which will fill DEPLOYER_PRIVATE_KEY
-    with a random private key in the .env file (then used on hardhat.config.ts)
-    You can run the `yarn account` command to check your balance in every network.
-  */
-  // // Deploy Vendor
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
+
+  // Get the deployed YourToken contract
   const yourToken = await hre.ethers.getContract<Contract>("YourToken", deployer);
   const yourTokenAddress = await yourToken.getAddress();
-  await deploy("Vendor", {
+
+  // Mint tokens to the deployer account (if your token contract has a mint function)
+  const mintAmount = hre.ethers.utils.parseUnits("10000", 18); // Adjust the amount and decimals as needed
+  await yourToken.mint(deployer, mintAmount); // Ensure your YourToken contract has a mint function
+
+  // Deploy the Vendor contract
+  const vendorDeployment = await deploy("Vendor", {
     from: deployer,
-    // Contract constructor arguments
     args: [yourTokenAddress],
     log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
-  const vendor = await hre.ethers.getContract<Contract>("Vendor", deployer);
-  const vendorAddress = await vendor.getAddress();
-  // // Transfer tokens to Vendor
-  await yourToken.transfer(vendorAddress, hre.ethers.parseEther("1000"));
-  // // Transfer contract ownership to your frontend address
-  await vendor.transferOwnership("0xd0D22EE2dbf27cc4747e6B4e2dBf1Ab2EE7ca437");
+
+  const vendorAddress = vendorDeployment.address;
+
+  // Transfer tokens to Vendor
+  await yourToken.transfer(vendorAddress, hre.ethers.utils.parseUnits("1000", 18)); // Transfer 1000 tokens
+
+  // Transfer contract ownership to your frontend address
+  await yourToken.transferOwnership("0xd0D22EE2dbf27cc4747e6B4e2dBf1Ab2EE7ca437");
 };
 
 export default deployVendor;
